28e3b0a6a4f29d29ad4fc306f086b710e01477ee
zion <nwpu409@gmail.com>
2020-11-05 22:40:18 +0800

    change layout

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Table } from 'antd';
+import { Table, Button, Layout, Menu } from 'antd';
 import 'antd/dist/antd.css';
 
 // eslint-disable-next-line no-unused-vars
 import mockData from './mock';
+
+const { Header, Content, Footer } = Layout;
 
 class App extends React.Component {
   state = {
@@ -38,16 +40,28 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="app-container">
-        <input type="file" onChange={this.onFileChange} />
-        <br />
-        <button onClick={this.onFileUpload}>Upload config file</button>
-        <br />
-        {this.state.tableData
-          ? <ConfigTable tbData={this.state.tableData}></ConfigTable>
-          : <div>please upload file</div>
-        }
-      </div>
+      <Layout className="app-container">
+        <Header>
+          {/* <input type="file" onChange={this.onFileChange} />
+          <br />
+          <Button onClick={this.onFileUpload}>Upload config file</Button> */}
+          <Menu theme="dark" mode="horizontal">
+            <Menu.Item key="1">加载配置</Menu.Item>
+            <Menu.Item key="2">加载 Excel</Menu.Item>
+            <Menu.Item key="3">设置配置</Menu.Item>
+            <Menu.Item key="4">设置模板</Menu.Item>
+            <Menu.Item key="5">保存配置</Menu.Item>
+            <Menu.Item key="6">另存配置</Menu.Item>
+          </Menu>
+        </Header>
+        <Content>
+          {this.state.tableData
+            ? <ConfigTable tbData={this.state.tableData}></ConfigTable>
+            : <h1 className="emptyProjectInfo">请打开配置文件所在文件夹</h1>
+          }
+        </Content>
+        <Footer style={{ textAlign: "center" }}>ShareE ©2020 YCD</Footer>
+      </Layout>
     );
   }
 }
@@ -57,7 +71,7 @@ class ConfigTable extends React.Component {
     const tableData = this.props.tbData;
     return (
       <div className="config-container">
-        <Table dataSource={tableData} columns={columns} />
+        <Table dataSource={tableData} columns={columns} pagination={false} />
       </div>
     );
   }
